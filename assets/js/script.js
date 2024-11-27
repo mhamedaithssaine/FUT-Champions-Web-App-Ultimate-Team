394991abd3dd941ca202b225ad2367cdf9572eaa
Mhamedaithssaine <mhamedaithssaine1@gmail.com>
2024-11-27 23:27:40 +0100

    modifier sur les carte des joueur, ajouter un function pour masquer e afficher les statisque apartir de vous choisi

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,29 +1,70 @@
-const btn = document.querySelector('.btn');
-const addPlayerForm = document.querySelector('.nonn');
+// affiche et masquer de formulaire 
+const btnAjouter = document.querySelector('.btn-ajouter');
+const formulaire = document.querySelector('.nonn');
 const btnCancel = document.querySelector('.btn-cancel');
 
 
-btn.addEventListener('click', () => {
-   addPlayer();
+btnAjouter.addEventListener('click', () => {
+   afficheForme();
 
 });
 
+//pour annuller le forme
+
 btnCancel.addEventListener('click',()=>{
-    addPlayerForm.classList.remove('nonn1');
-  addPlayerForm.classList.add('nonn');
+    formulaire.classList.remove('nonn1');
+  formulaire.classList.add('nonn');
   btnCancel.classList.remove('btn-cancel1')
   btnCancel.classList.add('btn-cancel')
-  btn.classList.remove('btn1')
-  btn.classList.add('btn')
+  btnAjouter.classList.remove('btn1')
+  btnAjouter.classList.add('btn')
 
-})
+});
 
-function addPlayer(){
-  addPlayerForm.classList.remove('nonn');
-  addPlayerForm.classList.add('nonn1');
+//pour affichage de forme
+
+function afficheForme(){
+  formulaire.classList.remove('nonn');
+  formulaire.classList.add('nonn1');
   btnCancel.classList.remove('btn-cancel')
   btnCancel.classList.add('btn-cancel1')
-  btn.classList.remove('btn')
-  btn.classList.add('btn1')
+  btnAjouter.classList.remove('btn')
+  btnAjouter.classList.add('btn1')
+
+}
+// affiche et masquer formulaire specifique en fonction de la position choisie
+
+document.getElementById("PositionPlayer").addEventListener("change",function(){
+  const selectPosition = this.value;
+  const principalStatique = document.getElementById("principalStatique")
+  const gardianStatique = document.getElementById("gkStatique")
+
+  if(selectPosition === "GK"){
+    gardianStatique.style.display="block";
+    principalStatique.style.display="none";
+  }else {
+    gardianStatique.style.display="none";
+    principalStatique.style.display="block";
+  }
+
+  console.log(selectPosition);
+});
+
+
+//fonction pour ajouter un joueur et recuperation des donner 
+
+//fonction de validation de formulaire 
+function validationForme(){
+  const namePlayer = document.getElementById("playerName");
+  const photoPlayer = document.getElementById("photoPlayer");
+  const positionPlayer = document.querySelector(".positionPlayerid");
+  const nationalityPlayer = document.getElementById("nationalityPlayer");
+  const DraeuNational = document.getElementById("DraeuNational");
+  const clubPlayer = document.getElementById("clubPlayer");
+  const logoClub = document.getElementById("logoClub");
+  const ratingPlayer = document.getElementById("ratingPlayer");
+
+
+
 
 }
